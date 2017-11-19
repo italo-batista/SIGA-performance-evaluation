16b14a289acd6e04b062001c964655126058517a
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2017-11-19 17:48:42 -0300

    Generate data using 1GB RAM

--- measurements/scripts/requests/request_get_80_reports_pdf.sh
+++ measurements/scripts/requests/request_get_80_reports_pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-id=2001
+id=8001
 filename="../../data/eds_ids.txt"
 "ID,TIPO_REQUISICAO,MOMENTO,TIMESTAMP" >> "../../data/request_get_80_reports_pdf.csv"
 
@@ -8,7 +8,7 @@ while read -r line
 do
     ed_id="$line"
     
-    for i in {1..10}
+    for i in {1..15}
 	do
 	  
 	  id=$(echo "scale=2; $id + 1"|bc)
--- measurements/scripts/requests/request_post_80_create_water_consumption.sh
+++ measurements/scripts/requests/request_post_80_create_water_consumption.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-id=1001
+id=4001
 filename="../../data/eds_ids.txt"
-"ID,TIPO_REQUISICAO,MOMENTO,TIMESTAMP" >> "../../data/request_post_180_create_water_consumption.csv"
+"ID,TIPO_REQUISICAO,MOMENTO,TIMESTAMP" >> "../../data/request_post_80_create_water_consumption.csv"
 
 while read -r line
 do
@@ -15,9 +15,9 @@ do
 	  data="2017-11-$i 12:00:00"
 	  consumo=$(( ( RANDOM % 10 )  + 1 ))
 	  
-	  echo $id",POST,BEGIN,"$(date +%s%N) >> "../../data/request_post_180_create_water_consumption.csv"
+	  echo $id",POST,BEGIN,"$(date +%s%N) >> "../../data/request_post_80_create_water_consumption.csv"
 	  curl -g -H "Content-Type:application/json" -X POST -d '{ "data":"'"$data"'","consumo":"'"$consumo"'" }' localhost:3000/edificio/"$ed_id"/consumo/new	  
-	  echo $id",POST,END,"$(date +%s%N) >> "../../data/request_post_180_create_water_consumption.csv"
+	  echo $id",POST,END,"$(date +%s%N) >> "../../data/request_post_80_create_water_consumption.csv"
 	  	  
 	  sleep 0.75s
 	done    

0f795dd0b8e850b63974601b1e2d228e80bf1505
André Lopes <oandrevictor@gmail.com>
2017-11-30 19:08:33 -0300

    Script is now generic

--- measurements/scripts/all.sh
+++ measurements/scripts/all.sh
@@ -16,10 +16,10 @@ source "./request_get_80_consumptions.sh"
 echo "GET 80 DONE"
 
 
-mv "../../data/request_get_180_consumptions.csv" "../../data/1GB/A5/"
-mv "../../data/request_get_80_consumptions.csv" "../../data/1GB/A5/"
-mv "../../data/request_post_80_create_water_consumption.csv" "../../data/1GB/A5/"
-mv "../../data/request_post_180_create_water_consumption.csv" "../../data/1GB/A5/"
+mv "../../data/request_get_180_consumptions.csv" "../../data/$1B/$2/"
+mv "../../data/request_get_80_consumptions.csv" "../../data/$1B/$2/"
+mv "../../data/request_post_80_create_water_consumption.csv" "../../data/$1B/$2/"
+mv "../../data/request_post_180_create_water_consumption.csv" "../../data/$1B/$2/"
 cd -
 sh "./delete_eds.sh"
 rm -f "../data/eds_ids.txt"
